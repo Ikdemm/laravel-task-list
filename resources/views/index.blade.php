25d30d7399ad61117a7a7f19e3a70bee9e41487e
Ikdemm <ikdem.benmbarek@aiesec.net>
2024-08-23 07:41:35 -0500

    add app layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,16 @@
-<h1>Welcome</h1>
-<p>You have {{ count($tasks) }} Tasks</p>
-<div>
-    <ul>
-        @forelse ($tasks as $task)
-        <li><a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a></li>
-        @empty
-        <span>No Tasks to display</span>
-        @endforelse
-    </ul>
-</div>
+@extends('layouts.app')
+
+@section('title', 'The list of Tasks')
+
+@section('content')
+    <p>You have {{ count($tasks) }} Tasks</p>
+    <div>
+        <ul>
+            @forelse ($tasks as $task)
+            <li><a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a></li>
+            @empty
+            <span>No Tasks to display</span>
+            @endforelse
+        </ul>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 class Task
@@ -54,14 +55,20 @@ $tasks = [
     ),
 ];
 
-Route::get('/', function () use ($tasks) {
+Route::get('/tasks', function () use ($tasks) {
     return view("index", [
         'tasks' => $tasks
     ]);
 })->name('tasks.index');
 
-Route::get("/tasks/{id}", function ($id) {
+Route::get("/tasks/{id}", function ($id) use ($tasks) {
+    $task = collect($tasks)->firstWhere('id', $id);
+
+    if (!$task) {
+        abort(Response::HTTP_NOT_FOUND);
+    }
+
     return view("show", [
-        'id' => $id
+        'task' => $task
     ]);
 })->name('tasks.show');
